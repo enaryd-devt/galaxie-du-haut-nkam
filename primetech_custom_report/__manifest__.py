2e8e51df543799f9d72b8cd5cb596d6307e0b43b
Dirane Ngousse <enaryd123@gmail.com>
2026-09-15 20:52:18 +0100

    update

---
+++ primetech_custom_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'PrimeTech Reports custom',
+    'version': '18.0.1.0.0',
+    'summary': 'Custom layout report for Odoo 18',
+    'author': 'PrimeTech',
+    'depends': ['account', 'stock'],
+    'data': [
+        "views/custom_report_invoice.xml",
+        "views/inventory_count_report.xml",
+        "views/custom_receipt_payment.xml",
+        "views/custom_saleorder.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
